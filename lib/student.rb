1889b6b3ba0cb75b9a994b54b38cbb7d4b06b10b
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-13 23:16:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
       SELECT * FROM Students WHERE grade < ?
     SQL
     
-    students = DB[:conn].execute(sql, "12")
+    DB[:conn].execute(sql, "12")
   end
   
   def self.first_X_students_in_grade_10(num)
